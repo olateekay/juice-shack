91f60cbdc960d49d58b2c51f9fa72b07b278ca19
mahsa <upmahjour@gmail.com>
2024-11-09 17:31:46 +0000

    upload all

--- upload-reports.py
+++ upload-reports.py
@@ -1,4 +1,16 @@
 import requests
+import sys
+
+file_name = sys.argv[1]
+scan_type = ''
+
+if file_name == 'gitleaks.json':
+    scan_type = "Gitleaks Scan"
+elif file_name == 'njsscan.sarif':
+    scan_type = "SARIF"
+elif file_name == 'semgrep.json':
+    scan_type = 'Semgrep JSON Report'    
+
 
 headers = {
     'Authorization': 'Token 5d9b9e650bcfdc45888b3e6da582ca621b4cefd1'
@@ -9,14 +21,14 @@ url = 'https://demo.defectdojo.org/api/v2/import-scan/'
 data = {
     'active': True,
     'verified': True,
-    'scan_type': 'Gitleaks Scan',
+    'scan_type': scan_type,
     'minimum-severity': 'Low',
     'engagement': 19
 
 }
 
 files = {
-    'file' : open('gitleaks.json', 'rb')
+    'file' : open(file_name, 'rb')
 }
 
 response = requests.post(url, headers=headers, data=data, files=files)
