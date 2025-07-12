ec418fc91f88b4eac314400401328f7aeee1937f
Nicholas  Kiprono <belliotnick@gmail.com>
2025-07-12 16:27:37 +0000

    added upload script

---
+++ upload-reports.py
@@ -0,0 +1,38 @@
+import requests
+import sys
+
+file_name = sys.argv[1]
+scan_type = ''
+
+if file_name == 'gitleaks.json':
+    scan_type = 'Gitleaks Scan'
+elif file_name == 'njsscan.sarif':
+    scan_type = 'SARIF'
+elif file_name == 'semgrep.json':
+    scan_type = 'Semgrep JSON Report'
+
+
+headers = {
+    'Authorization': 'Token 79bf8620dde606eee2f0fc593d6e16d9054fbc49'
+}
+
+url = 'https://demo.defectdojo.org/api/v2/import-scan/'
+
+data = {
+    'active': True,
+    'verified': True,
+    'scan_type': scan_type,
+    'minimum_severity': 'Low',
+    'engagement': 19
+}
+
+files = {
+    'file': open(file_name, 'rb')
+}
+
+response = requests.post(url, headers=headers, data=data, files=files)
+
+if response.status_code == 201:
+    print('Scan results imported successfully')
+else:
+    print(f'Failed to import scan results: {response.content}')
